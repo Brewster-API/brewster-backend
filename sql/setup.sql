d288fb7e9a9870e8d8003826aac202dafbf0f5a2
Kyle Mayer <x@kylemayer.dev>
2021-08-18 13:43:25 -0700

    shops SQL table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,12 +1,23 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS shops CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username TEXT NOT NULL,
     email TEXT NOT NULL,
     password_hash TEXT NOT NULL,
-    favorites TEXT [],
+    fav_id TEXT [] REFERENCES favorites(id)
 );
+
+CREATE TABLE shops (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    name TEXT NOT NULL,
+    location TEXT NOT NULL,
+    menu_id TEXT NOT NULL [] REFERENCES menus(id)
+)
 
 
 
