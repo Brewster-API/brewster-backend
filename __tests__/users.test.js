2021cf0f4b7fa0e8dbde0633e4fbe97feb2c7671
Kyle Mayer <x@kylemayer.dev>
2021-08-19 15:37:06 -0700

    user authenticate bcryt setup

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -3,6 +3,7 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
+
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
