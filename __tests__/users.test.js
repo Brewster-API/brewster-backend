db4de0ad1413cd1c9762e83db2f0ea67f4384cdc
Kyle Mayer <x@kylemayer.dev>
2021-08-19 14:51:45 -0700

    users post test setup

---
+++ __tests__/users.test.js
@@ -0,0 +1,20 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('demo routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a user via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: 'CupAJoe', email: 'cupajoe@aol.com', password: 'coffee123' });
+
+    expect(res.body).toEqual({ id: '1', username: 'CupAJoe', email: 'cupajoe@aol.com' });
+  });
+
+
+});
