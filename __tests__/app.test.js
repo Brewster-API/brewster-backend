7abb52d104bb7f5570a5c5f65c7c074b52320d96
Derek Hudgins <hudgins.derek@gmail.com>
2021-08-19 13:33:00 -0700

    get all drinks failed perfectly

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,8 +7,44 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  it('returns all drinks Via GET', async () => {
+    const drink1 = {
+      drinkName: 'Latte',
+      brew: 'Espresso',
+      description:
+        'As the most popular coffee drink out there, the latte is comprised of a shot of espresso and steamed milk with just a touch of foam. It can be ordered plain or with a flavor shot of anything from vanilla to pumpkin spice.',
+      ingredients: ['Espresso', 'Steamed Milk'],
+    };
+    const drink2 = {
+      drinkName: 'Americano',
+      brew: 'Espresso',
+      description:
+        'With a similar flavor to black coffee, the americano consists of an espresso shot diluted in hot water.',
+      ingredients: ['Espresso', 'Hot Water'],
+    };
+    const drink3 = {
+      drinkName: 'Black',
+      brew: 'Drip',
+      description:
+        'Black coffee is as simple as it gets with ground coffee beans steeped in hot water, served warm. And if you want to sound fancy, you can call black coffee by its proper name: cafe noir',
+      ingredients: ['Coffee'],
+    };
+    const response1 = await request(app).post('/api/v1/drinks').send(drink1);
+    const response2 = await request(app).post('/api/v1/drinks').send(drink2);
+    const response3 = await request(app).post('/api/v1/drinks').send(drink3);
 
+    expect([response1.body, response2.body, response3.body]).toEqual([{
+      id: '1',
+      ...drink1,
+    },
+    {
+      id: '2',
+      ...drink2,
+    },
+    {
+      id: '3',
+      ...drink3,
+    }]);
 
-
-
+  });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -4,14 +4,12 @@ CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username TEXT NOT NULL,
     email TEXT NOT NULL,
-    password_hash TEXT NOT NULL,
-    fav_id TEXT [] REFERENCES favorites(id)
+    password_hash TEXT NOT NULL
 );
 CREATE TABLE drinks (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     drink_name TEXT NOT NULL,
     brew TEXT NOT NULL,
-    temperature BOOL NOT NULL,
     drink_description TEXT NOT NULL,
     ingredients TEXT []
 );
