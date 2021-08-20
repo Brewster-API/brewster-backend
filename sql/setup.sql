62fe28a207e485b5c9f75f834b53341de567426b
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-08-20 13:15:31 -0400

    added 2 more tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS users CASCADE;
-
 DROP TABLE IF EXISTS drinks CASCADE;
+DROP TABLE IF EXISTS favorites CASCADE;
+DROP TABLE IF EXISTS favorite_drink CASCADE; 
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,6 +10,19 @@ CREATE TABLE users (
     password_hash TEXT NOT NULL
 );
 
+CREATE TABLE favorites (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    drink_name TEXT NOT NULL,
+    user_id REFERENCES users(id)
+)
+
+CREATE TABLE favorite_drink (
+    favorite_id BIGINT, 
+    drink_id BIGINT,
+    FOREIGN KEY (favorite_id) REFERENCES favorites(id), 
+    FOREIGN KEY (drink_id) REFERENCES drinks(id)
+)
+
 CREATE TABLE drinks (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     drink_name TEXT NOT NULL,
@@ -16,3 +30,5 @@ CREATE TABLE drinks (
     drink_description TEXT NOT NULL,
     ingredients TEXT []
 );
+
+
