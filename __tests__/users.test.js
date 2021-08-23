3ed4b67333f3c59fac7d160326b8358d61b26ece
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-08-23 14:16:39 -0400

    created a failing test for users choice

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -101,4 +101,59 @@ describe('demo routes', () => {
       email: 'cupajoe@aol.com',
     });
   });
+
+  it('users can add drinks under their account via POST', async () => {
+    const user = {
+      username: 'MochaJoe',
+      email: 'mochaJo@aol.com',
+      password: 'coffee123',
+    };
+
+    const userDrink = {
+      drinkName: 'Latte',
+      brew: 'Espresso',
+      description: 'xyz',
+      ingredients: ['Espresso', 'Milk'],
+    };
+
+    const userInfo = await request(app).post('/api/v1/auth/login').send(user);
+    const res = await request(app)
+      .post('/api/v1/auth/user/drinks')
+      .send(userDrink, user.id);
+
+    expect(res.body).toEqual({
+      id: res.id,
+      ...userDrink,
+      user: userInfo.id, 
+    }); 
+  });
+  
+  // it('User can update their own post via PUT', async () => {
+  //   const user = {
+  //     username: 'MochaJoe',
+  //     email: 'mochaJo@aol.com',
+  //     password: 'coffee123',
+  //   };
+
+  //   const userDrink = {
+  //     drinkName: 'Latte',
+  //     brew: 'Espresso',
+  //     description: 'xyz',
+  //     ingredients: ['Espresso', 'Milk'],
+  //   };
+    
+  //   const updateDrink = {
+  //     drinkName: 'ChaiLatte',
+  //     brew: 'Espresso',
+  //     description: 'xyz',
+  //     ingredients: ['Espresso', 'Milk', 'Chai'],
+  //   };
+
+
+
+  //   const drinkInfo = await request(app).post('/api/v1/auth/login').send(user);
+  //   await Drink.insert(userDrink);
+  //   const  updatedDrinkInfo = await request(app).put()
+        
+  // }); 
 });
