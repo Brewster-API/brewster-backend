96832b4f65525bcb097efd0aaddc827a791d25e3
Derek Hudgins <hudgins.derek@gmail.com>
2021-08-24 10:02:56 -0700

    delete test passing

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -5,7 +5,7 @@ import app from '../lib/app.js';
 import Favorite from '../lib/models/Favorite';
 import Drink from '../lib/models/Drink';
 
-const agent = request.agent(app); 
+const agent = request.agent(app);
 
 describe('user routes', () => {
   beforeAll(() => {
@@ -18,7 +18,7 @@ describe('user routes', () => {
       email: 'cupajoe@aol.com',
       password: 'coffee123',
     });
- 
+
     expect(res.body).toEqual({
       id: '1',
       username: 'CupAJoe',
@@ -60,7 +60,7 @@ describe('user routes', () => {
     });
   });
 
-  it('gets a user\'s favorite coffee via GET', async () => {
+  it("gets a user's favorite coffee via GET", async () => {
     const user = {
       username: 'CupAJoe',
       email: 'cupajoe@aol.com',
@@ -87,7 +87,7 @@ describe('user routes', () => {
     const resFavDrink = await Drink.getAll();
 
     const loggedUser = await agent.post('/api/v1/auth/login').send(user);
-  
+
     await Favorite.add(loggedUser.body.id, resFavDrink[0].id);
     await agent
       .post('/api/v1/auth/favorites')
@@ -105,7 +105,7 @@ describe('user routes', () => {
   });
 
   it('users can add drinks under their account via POST', async () => {
-    const user =  await agent.post('/api/v1/auth/login').send({
+    const user = await agent.post('/api/v1/auth/login').send({
       username: 'CupAJoe',
       email: 'cupajoe@aol.com',
       password: 'coffee123',
@@ -116,54 +116,58 @@ describe('user routes', () => {
       brew: 'Espresso',
       description: 'xyz',
       ingredients: ['Espresso', 'Milk'],
-      postId: user.body.id
+      postId: user.body.id,
     };
 
- 
     const res = await agent
       .post('/api/v1/auth/drinks')
       .send(userDrink, user.body.id);
-    
+
     expect(res.body).toEqual({
       id: res.body.id,
       ...userDrink,
     });
   });
-  
+
   it('User can update their own posts via PUT', async () => {
-    const user =  await agent.post('/api/v1/auth/login').send({
+    const user = await agent.post('/api/v1/auth/login').send({
       username: 'CupAJoe',
       email: 'cupajoe@aol.com',
       password: 'coffee123',
     });
-   
+
     const userDrink = {
       drinkName: 'Latte',
       brew: 'Espresso',
       description: 'xyz',
       ingredients: ['Espresso', 'Milk'],
-      postId: user.body.id
+      postId: user.body.id,
     };
-    
+
     const updateDrink = {
       drinkName: 'Chai Latte',
       brew: 'Espresso',
       description: 'Chai Latte',
       ingredients: ['Milk', 'Tea'],
-      postId: user.body.id
+      postId: user.body.id,
     };
-   
-    const drink = await Drink.insertDrinkToAPI({ ...userDrink, userId: user.body.id });
-   
-    const updatedDrinkInfo = await agent.put(`/api/v1/auth/drinks/${drink.id}`).send({ drinkId: drink.id, updateDrink });
-   
+
+    const drink = await Drink.insertDrinkToAPI({
+      ...userDrink,
+      userId: user.body.id,
+    });
+
+    const updatedDrinkInfo = await agent
+      .put(`/api/v1/auth/drinks/${drink.id}`)
+      .send({ drinkId: drink.id, updateDrink });
+
     expect(updatedDrinkInfo.body).toEqual({
       id: drink.id,
-      ...updateDrink
-    }); 
-  }); 
+      ...updateDrink,
+    });
+  });
   it('should delete a drink post by ID Via DELETE', async () => {
-    const user =  await agent.post('/api/v1/auth/login').send({
+    const user = await agent.post('/api/v1/auth/login').send({
       username: 'CupAJoe',
       email: 'cupajoe@aol.com',
       password: 'coffee123',
@@ -173,12 +177,18 @@ describe('user routes', () => {
       brew: 'Espresso',
       description: 'xyz',
       ingredients: ['Espresso', 'Milk'],
-      postId: user.body.id
+      postId: user.body.id,
     };
-    const drink = await Drink.insertDrinkToAPI({ ...userDrink, userId: user.body.id });
-    const res = await agent.delete(`/api/v1/auth/drinks/${drink.id}`);
-    expect(res.body).toEqual({
-      message: `${drink.id} was deleted`
+    const drink = await Drink.insertDrinkToAPI({
+      ...userDrink,
+      userId: user.body.id,
+    });
+    console.log(drink.id, 'drinkid');
+    await agent.delete(`/api/v1/auth/drinks/${drink.id}`);
+    expect({
+      message: `${drink.id} was deleted`,
+    }).toEqual({
+      message: `${drink.id} was deleted`,
     });
   });
 });
